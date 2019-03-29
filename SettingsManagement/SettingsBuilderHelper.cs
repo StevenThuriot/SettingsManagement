7f99758d29c24d6a5ec528bc8fdd4fb662b5bb9d
Steven Jack Thuriot <steven@thuriot.be>
2019-03-29 23:00:38 +0100

    Created Helper that will add some caching

---
+++ SettingsManagement/SettingsBuilderHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SettingsManagement
+{
+    static class SettingsBuilderHelper
+    {
+        static readonly IDictionary<Type, MethodInfo> _initMethods = new Dictionary<Type, MethodInfo>();
+        static readonly IDictionary<Type, MethodInfo> _creationMethods = new Dictionary<Type, MethodInfo>();
+        static readonly IDictionary<Type, MethodInfo> _createAndParseMethods = new Dictionary<Type, MethodInfo>();
+
+        public static MethodInfo ResolveInit(Type type)
+        {
+            if (!_initMethods.TryGetValue(type, out var info))
+            {
+                _initMethods[type] = info = typeof(SettingsBuilder<>).MakeGenericType(type).GetMethod("Init");
+            }
+
+            return info;
+        }
+		
+        public static MethodInfo ResolveCreate(Type type)
+        {
+            if (!_creationMethods.TryGetValue(type, out var info))
+            {
+                _creationMethods[type] = info = typeof(SettingsBuilder<>).MakeGenericType(type).GetMethod("Create");
+            }
+
+            return info;
+        }
+
+        public static MethodInfo ResolveParseAndCreate(Type type)
+        {
+            if (!_createAndParseMethods.TryGetValue(type, out var info))
+            {
+                _createAndParseMethods[type] = info = typeof(SettingsBuilder<>).MakeGenericType(type).GetMethod("ParseAndCreate");
+            }
+
+            return info;
+        }
+    }
+}
